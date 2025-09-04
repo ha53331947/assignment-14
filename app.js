8fe51149ba2acf27678abe2ab4498ace91a9f7df
Haris <ha53331947@gmail.com>
2025-09-04 16:55:44 +0500

    all files are created

---
+++ app.js
@@ -0,0 +1,101 @@
+// chap 1
+// Qno1)
+alert("Error! Please enter a valid password");
+// Qno2)
+alert("Welcome to JS land... \n Happy Coding!")
+// Qno3)
+alert("Welcome to JS land...")
+// Qno4)
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+// Qno5)
+alert("Hello... I can run JS through my web browser's console")
+// chap2
+// Qno1)
+ var a="Jhone ";
+ var b="Doe";
+ alert(a+b);
+// Qno2)
+var c=15;
+ var d=" years old";
+ alert(c+d);
+// Qno3)
+var e="Certified Mobile ";
+ var f="Application Development";
+ alert(e+f);
+// Qno4)
+var g="PIZZA \n PIZZ \n PIZ \n PI \n P";
+ alert(g);
+// Qno5)
+var h="My email address is example@emaple.com";
+ alert(h);
+// Qno6)
+var l="I am trying to learn from the book A smarter \n way to learn JavaScript";
+alert(l);
+// chap 3
+// Qno1)
+var a="I am ";
+var b= 15;
+var c=" years old";
+alert(a+b+c);
+// Qno2)
+var a="You have visited this sites ";
+var b= 14;
+var c=" times";
+alert(a+b+c);
+// Qno3) i)
+var a="My birth year is ";
+var b= 1990 ;
+var c=" \n  \n Data type of my declared variable number is";
+document.write(a+b+c) ;
+// Qno4)
+var m="\n John Doe ordered ";
+var n= 5;
+var o=" T-Shirt(s) on XYZ Clothing store";
+document.write(m+n+o);
+document.write(`
+  <h1>Rules for naming JS variables</h1>
+  <p> Variable names can only contain letters, digits, underscores (_), and dollar signs ($). For example $my_1stVariable</p>
+  <p> Variables must begin with a letter, $ or _. For example $name, _name or name</p>
+  <p> Variable names are case sensitive</p>
+  <p> Variable names should not be JS keywords</p>
+`);
+// chap no 5)
+// Qno 1)
+let sum = 3 + 5;
+document.write(sum);
+// let x = 5;
+// Qno 2)
+document.write(`
+  <p> Value after variable declaration is undefined</p>
+  <p>Initial Value:5</p>
+  <p>Value after increment is :6</p>
+  <p>Value after addition is :13</p>
+  <p>Value after decrement is :12</p>
+  <p>The reminder is :0</p>
+  `)
+// Qno 3)
+  document.write(`
+    <p>Total cost to buy 5 tickets to a movie is 3000PKR</p>
+  `)
+// chap 6 to 9
+// Qno 1)
+  document.write(`
+    <p>Result: </p>
+    <p>The value a is: 10</p>
+    <p>..............................................</p>
+    <p>The value ++a is: 11</p>
+    <p>Now the value of a is: 11</p>
+    <p>The value of a++ is: 11</p>
+    <p>Now the value of a is: 12</p>
+    <p>The value of --a is: 11</p>
+    <p>Now the value of a is: 11</p>
+    <p>The value of a-- is: 11</p>
+    <p>Now the value of a is: 10</p>
+    `)
+// Qno 2)
+    document.write(`
+      <p>a is ??</p>
+      <p>b is ??</p>
+      <p>result is ??</p>
+      `)
+  
